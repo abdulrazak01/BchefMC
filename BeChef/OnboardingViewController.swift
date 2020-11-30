b53494f5410f8b951dfc21eb75f527d39227c8f6
riian26 <riianofficial@gmail.com>
2020-12-01 03:43:53 +0700

    Onboarding Screen FIxed

--- BeChef/OnboardingViewController.swift
+++ BeChef/OnboardingViewController.swift
@@ -11,6 +11,8 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var holderView: UIView!
     let scrollview = UIScrollView()
+    var lewatiItem = UIButton()
+    var currentStep: Int = 0
     
     private let pageControl: UIPageControl = {
         let pageControl = UIPageControl()
@@ -49,31 +51,48 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
             let titles = ["Ketika lapar datang menyerang", "Pilih jagoan-jagoan terbaikmu", "Lancarkan jurus-jurusnya"]
             let title = ["4 pahlawan ini siap membantumu melawan monster “Lapar”!", "Pilih dan kombinasikan jurus mereka untuk mengalahkan monster “Lapar”!", "Bersama jagoanmu, lancarkan jurusnya, dan kalahkan monster “Lapar”!"]
             
-            pageControl.frame = CGRect(x: 10, y: holderView.frame.size.height-175, width: holderView.frame.size.width-20, height: 70)
+            pageControl.frame = CGRect(x: 10, y: holderView.frame.size.height-180, width: holderView.frame.size.width-20, height: 70)
            
            
             for x in 0..<3 {
                 let pageView = UIView(frame: CGRect(x: CGFloat(x) * (holderView.frame.size.width), y: 0, width: holderView.frame.size.width, height: holderView.frame.size.height))
                 scrollview.addSubview(pageView)
                 
-                let label = UILabel(frame: CGRect(x: 10, y: 500, width: pageView.frame.size.width-20, height: 120))
-                let label1 = UILabel(frame: CGRect(x: 20, y: 550, width: pageView.frame.size.width-40, height: 120))
+                let label = UILabel(frame: CGRect(x: 10, y: 450, width: pageView.frame.size.width-20, height: 120))
                 
-                let imageView = UIImageView(frame: CGRect(x: 10, y: 0, width: pageView.frame.size.width-20, height: pageView.frame.size.height-60-130-15))
+                
+                let label1 = UILabel(frame: CGRect(x: 20, y: 490, width: pageView.frame.size.width-40, height: 120))
+                
+                
+                let imageView = UIImageView(frame: CGRect(x: 10, y: -20, width: pageView.frame.size.width-20, height: pageView.frame.size.height-60-130-15))
     
-                let button = UIButton(frame: CGRect(x: 40, y: holderView.frame.size.height-100, width: holderView.frame.size.width-80, height: 50))
+                let button = UIButton(frame: CGRect(x: 40, y: holderView.frame.size.height-100, width: holderView.frame.size.width-80, height: 57))
                 
     
+                lewatiItem = UIButton(frame: CGRect(x: 10, y: holderView.frame.size.height-250, width: holderView.frame.size.width-20, height: 70))
+                let attributedString = NSAttributedString(string: NSLocalizedString("Lewati", comment: ""), attributes:[
+                    NSAttributedString.Key.font : UIFont(name: "SFProRounded-Regular", size: 17)!,
+                    NSAttributedString.Key.foregroundColor : UIColor.white,
+                    NSAttributedString.Key.underlineStyle:1.0
+                ])
+                lewatiItem.setAttributedTitle(attributedString, for: .normal)
+                lewatiItem.backgroundColor = .none
+                lewatiItem.setTitle("Lewati", for: .normal)
+                lewatiItem.setTitleColor(.white, for: .normal)
+                lewatiItem.addTarget(self, action: #selector(pressedRight(_ :)), for: .touchUpInside)
+                lewatiItem.layer.masksToBounds = true
+                pageView.addSubview(lewatiItem)
+                
+                
                 label.textAlignment = .center
-                label.font = UIFont(name: "SF Pro Rounded", size: 20)
-                label.font = UIFont.boldSystemFont(ofSize: 20)
+                label.font = UIFont(name: "SFProRounded-Bold", size: 20)
                 label.textColor = .white
                 label.numberOfLines = 0
                 pageView.addSubview(label)
                 label.text = titles[x]
                 
                 label1.textAlignment = .center
-                label1.font = UIFont(name: "SF Pro Rounded", size: 17)
+                label1.font = UIFont(name: "SFProRounded-Regular", size: 17)
                 label1.textColor = .white
                 label1.numberOfLines = 0
                 pageView.addSubview(label1)
@@ -92,11 +111,14 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
                 
                 pageControl.numberOfPages = title.count
                 if x == 2 {
-                    button.setTitle("Mulai", for: .normal)
+                    button.setTitle("Mulai Kuy!", for: .normal)
+                    button.titleLabel?.font = UIFont(name: "SFProRounded-Bold", size: 32)
                     button.isHidden = false
+                    lewatiItem.isHidden = true
                 }
                     
                 pageControl.addTarget(self, action: #selector(pageControlDidChange(_:)), for: .valueChanged)
+                pageControl.currentPageIndicatorTintColor = .yellow
                 
                 
                 button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
@@ -105,26 +127,27 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
                 holderView.addSubview(pageControl)
                 pageView.addSubview(button)
                 
-                
-//                button1.setTitleColor(.white, for: .normal)
-//                button1.backgroundColor = .none
-//                button1.setTitle("Lewati", for: .normal)
-//                button1.layer.cornerRadius = 12
-//                button1.tag = x+1
-//                if x == 2 {
-//                    button1.isHidden = true
-//                }
-//                button.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
-//                pageView.addSubview(button)
             }
-//            holderView.addSubview(pageControl)
-            
-            
+
             scrollview.contentSize = CGSize(width: holderView.frame.size.width*3, height: 0)
             scrollview.isPagingEnabled = true
             
         
             
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+        currentStep = Int(pageNumber)
+        
+    }
+    
+    @objc func pressedRight(_ sender: UIButton) {
+        let contentOffset = CGPoint(x: holderView.frame.size.width * CGFloat(2), y: scrollview.contentOffset.y)
+        scrollview.setContentOffset(contentOffset, animated: true)
+          print("Lewati Press")
+
     }
     
     @objc func pageControlDidChange(_ sender: UIPageControl){
@@ -133,41 +156,22 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     }
     
     
+    
     @objc func didTapButton(_ button: UIButton){
         guard button.tag < 3  else {
-            
             let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
             guard let destinationViewController = mainStoryboard.instantiateViewController(withIdentifier: "aaa") as? UINavigationController else {
                 print("ga nemu")
                 return
             }
-            destinationViewController.modalPresentationStyle = .fullScreen
-            present(destinationViewController, animated: true, completion: nil)
-            
-            
-//            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-//            let newViewController = storyBoard.instantiateViewController(withIdentifier: "Main") as! ViewController
-//
-//            self.present(newViewController, animated: true, completion: nil)
-            
-            return
+        destinationViewController.modalPresentationStyle = .fullScreen
+        present(destinationViewController, animated: true, completion: nil)
+    
+         return
         }
         scrollview.setContentOffset(CGPoint(x: holderView.frame.size.width * CGFloat(button.tag), y: 0), animated: true)
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 }
-//
-//extension ViewController: UIScrollViewDelegate{
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-////        pageControl.currentpage = Int(floorf(Float(scrollView.contentOffset.x) / Float(scrollView.frame.size.widht)))
-
